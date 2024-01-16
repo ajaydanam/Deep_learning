db2cab3107a415da8a3141a1845f3891be9a93c5
ajaydanam <156715811+ajaydanam@users.noreply.github.com>
2024-01-15 18:25:35 -0600

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,19 @@
+
+def string_operation():
+    try:
+        input1 = str(input("Enter your string here:"))
+        if input1 != '' and input1 is not None and input1.isspace() != True and input1.isnumeric() != True:
+            # Deleting at least 2 characters
+            output = input1[:-2]
+            # reversal of the string after
+
+            output = output[::-1]
+
+            print(output)
+        else:
+            print("please enter a valid string")
+    except Exception as error:
+        print("Error occured {}".format(error))
+if __name__ == "__main__":
+    string_operation()
+
